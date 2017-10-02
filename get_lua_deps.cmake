9dff8a8f6aeaeb5210f7402f30638f244c46fff8
Henry Borchers <henryborchers@yahoo.com>
2017-10-02 11:29:00 -0500

    split lua_deps into lualuasockets and luaBusted

--- get_lua_deps.cmake
+++ get_lua_deps.cmake
@@ -1,20 +1,25 @@
-add_custom_target(lua_deps
+add_custom_target(lualuasockets
         DEPENDS luarocks::luarocks
-        COMMENT "Getting deps from lua rocks"
+        COMMENT "Adding Luasocks"
         )
+
 
 add_custom_command(
         POST_BUILD
-        TARGET lua_deps
+        TARGET lualuasockets
         COMMAND luarocks::luarocks
         ARGS install --tree ${CMAKE_BINARY_DIR}/ luasocket
         COMMENT "Adding luasocket"
         BYPRODUCTS cmake-build-debug/lua_modules/lib/luarocks/rocks/luasocket
 )
 
+add_custom_target(luaBusted
+        DEPENDS luarocks::luarocks
+        COMMENT "Adding Luasocks"
+        )
 add_custom_command(
         POST_BUILD
-        TARGET lua_deps
+        TARGET luaBusted
         COMMAND luarocks::luarocks
         ARGS install --tree ${CMAKE_BINARY_DIR} busted
         COMMENT "Adding busted"
@@ -24,7 +29,7 @@ add_executable(busted::busted IMPORTED)
 set_target_properties(busted::busted PROPERTIES
         IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/busted${CMAKE_EXECUTABLE_SUFFIX}
         )
-add_dependencies(busted::busted lua_deps)
+add_dependencies(busted::busted luaBusted)
 install(DIRECTORY ${CMAKE_BINARY_DIR}/lib/ DESTINATION lib)
 install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/ DESTINATION bin)
 install(DIRECTORY ${CMAKE_BINARY_DIR}/share/ DESTINATION share)
