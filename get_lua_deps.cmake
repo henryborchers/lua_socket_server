efff6f0428d2337c6cd99ac83af0d21fe9c07fcb
Henry Borchers <henryborchers@yahoo.com>
2017-10-08 18:33:15 -0500

    luarocks::luarocks to lua::luarocks

--- get_lua_deps.cmake
+++ get_lua_deps.cmake
@@ -1,5 +1,5 @@
 add_custom_target(lualuasockets
-        DEPENDS luarocks::luarocks
+        DEPENDS lua::luarocks
         COMMENT "Adding Luasocks"
         )
 
@@ -7,25 +7,26 @@ add_custom_target(lualuasockets
 add_custom_command(
         POST_BUILD
         TARGET lualuasockets
-        COMMAND luarocks::luarocks
+        COMMAND lua::luarocks
         ARGS install --tree ${CMAKE_BINARY_DIR}/ luasocket
         COMMENT "Adding luasocket"
         BYPRODUCTS cmake-build-debug/lua_modules/lib/luarocks/rocks/luasocket
 )
-
+add_dependencies(lualuasockets lua::luarocks )
 add_custom_target(luaBusted
-        DEPENDS luarocks::luarocks
+        DEPENDS lua::luarocks
         COMMENT "Adding Luasocks"
         )
 add_custom_command(
-        POST_BUILD
+#        POST_BUILD
         TARGET luaBusted
-        COMMAND luarocks::luarocks
+        COMMAND lua::luarocks
         ARGS install --tree ${CMAKE_BINARY_DIR} busted
         COMMENT "Adding busted"
         BYPRODUCTS cmake-build-debug/lua_modules/lib/luarocks/rocks/busted
 )
 add_executable(busted::busted IMPORTED)
+add_dependencies(luaBusted lua::luarocks)
 set_target_properties(busted::busted PROPERTIES
         IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/busted${CMAKE_EXECUTABLE_SUFFIX}
         )
