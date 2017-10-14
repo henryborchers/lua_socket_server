5115dd70c345a4f5a683f2cfec37f204fd5978ac
Henry Borchers <henryborchers@yahoo.com>
2017-10-14 17:17:57 -0500

    run the lua script

--- hello.cpp
+++ hello.cpp
@@ -16,6 +16,18 @@ using namespace std;
 int main(int argc, char *argv[]) {
     cout << "Hello World\n";
     lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    int ret;
+    if((ret = luaL_dofile(L, "share/myserver/set_paths.lua")) != 0){
+        cerr << lua_tostring(L, -1) << endl;
+        return ret;
+    };
+    if((ret = luaL_dofile(L, "share/myserver/main.lua")) != 0){
+        cerr << lua_tostring(L, -1) << endl;
+        return ret;
+    };
+
+    lua_close(L);
 
     cout << "All done\n";
     return 0;
