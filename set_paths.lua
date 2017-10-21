2e71569ff8ae745858465de8b117a8b14dea8500
Henry Borchers <henryborchers@yahoo.com>
2017-10-21 18:40:59 -0500

    no longer needed. made obsolete

--- set_paths.lua
+++
@@ -1,9 +0,0 @@
---
--- Created by IntelliJ IDEA.
--- User: hborcher
--- Date: 9/9/17
--- Time: 7:34 PM
--- To change this template use File | Settings | File Templates.
---
-local version = _VERSION:match("%d+%.%d+")
-package.path = 'lua_modules/share/lua/' .. version .. '/?.lua;lua_modules/share/lua/' .. version .. '/?/init.lua;share/lua/' .. version .. '/?.lua;source/?.lua;share/lua/' .. version .. '/?.lua;share/lua/' .. version .. '/?/init.lua;' .. package.path
