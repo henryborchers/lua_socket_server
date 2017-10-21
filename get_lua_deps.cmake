235e095b7b293dc1e1df4ea20e33428ee2803aaa
Henry Borchers <henryborchers@yahoo.com>
2017-10-21 14:33:24 -0500

    windows uses bat for extension on busted

--- get_lua_deps.cmake
+++ get_lua_deps.cmake
@@ -12,7 +12,6 @@ if (Lua_UseExternalLuaRocks)
             OUTPUT ${CMAKE_BINARY_DIR}/share/lua/${LUA_VERSION_STRING}/socket.lua
             COMMAND lua::luarocks
             ARGS install --tree ${CMAKE_BINARY_DIR}/ luasocket CC=${CMAKE_C_COMPILER} LD=${CMAKE_C_COMPILER}
-#            ARGS install --tree ${CMAKE_BINARY_DIR}/ luasocket
             COMMENT "Adding luasocket to ${CMAKE_BINARY_DIR}/"
     )
 
@@ -34,25 +33,24 @@ if (Lua_UseExternalLuaRocks)
             COMMENT "Adding Busted to ${CMAKE_BINARY_DIR}/"
     )
     add_dependencies(luaBusted lua::luarocks lua::sockets)
-    set_target_properties(busted::busted PROPERTIES
-            IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/busted.bat
-            )
-    add_dependencies(busted::busted luaBusted )
+    if (Win32)
+        set_target_properties(busted::busted PROPERTIES
+                IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/busted.bat
+                )
+    else ()
+        set_target_properties(busted::busted PROPERTIES
+                IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/busted
+                )
+    endif ()
+
+    add_dependencies(busted::busted luaBusted)
 else ()
     find_program(busted busted)
     set_target_properties(busted::busted PROPERTIES
             IMPORTED_LOCATION busted
             )
 endif ()
-#add_custom_command(
-##        POST_BUILD
-#        TARGET luaBusted
-#        COMMAND lua::luarocks
-#        ARGS install --tree ${CMAKE_BINARY_DIR} busted
-#        COMMENT "Adding busted"
-#        BYPRODUCTS cmake-build-debug/lua_modules/lib/luarocks/rocks/busted
 
-#)
 install(DIRECTORY ${CMAKE_BINARY_DIR}/lib/ DESTINATION lib)
 install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/ DESTINATION bin)
 install(DIRECTORY ${CMAKE_BINARY_DIR}/share/ DESTINATION share)
