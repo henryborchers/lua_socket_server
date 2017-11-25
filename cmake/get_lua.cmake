9b6fe31b480c94e7503c815f420035a13355d8e9
Henry Borchers <henryborchers@yahoo.com>
2017-11-24 21:42:06 -0600

    fixed dependency order

--- cmake/get_lua.cmake
+++ cmake/get_lua.cmake
@@ -7,24 +7,25 @@ if (PYTHONINTERP_FOUND)
             )
     add_custom_command(
             OUTPUT ${CMAKE_BINARY_DIR}/lua/bin/luarocks
-#            TARGET LuaRocks_hererocks
-            COMMAND ${PYTHON_EXECUTABLE}
-            ARGS -m hererocks ${CMAKE_BINARY_DIR}/lua --lua 5.3 --patch --luarocks 2.4.3
+            #            TARGET LuaRocks_hererocks
+            COMMAND ${PYTHON_EXECUTABLE} -m hererocks ${CMAKE_BINARY_DIR}/lua --lua 5.3 --patch --luarocks 2.4.3
             COMMENT "Adding lua and luarocks"
     )
-    if(WIN32)
+    if (WIN32)
         set_target_properties(lua::luarocks PROPERTIES
                 IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/lua/bin/luarocks.bat
                 )
-    else()
+    else ()
         set_target_properties(lua::luarocks PROPERTIES
-            IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/lua/bin/luarocks
-            )
-    endif()
+                IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/lua/bin/luarocks-5.3
+                )
+    endif ()
     set_target_properties(lua::lib PROPERTIES
             IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/lua/lib/${CMAKE_STATIC_LIBRARY_PREFIX}lua53${CMAKE_STATIC_LIBRARY_SUFFIX}
             INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_BINARY_DIR}/lua/include
             )
+    add_dependencies(lua::lib LuaRocks_hererocks)
     add_dependencies(lua::luarocks LuaRocks_hererocks)
     set(LUA_VERSION_STRING "5.3")
+    set(LUA_FOUND YES)
 endif ()
